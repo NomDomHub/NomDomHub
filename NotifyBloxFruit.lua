e6010192696ce64608914ee7dcdaf397105b1203
NomDomHub <duyanhkhanh0003@gmail.com>
2025-06-19 20:36:59 +0700

    Update NotifyBloxFruit.lua

--- NotifyBloxFruit.lua
+++ NotifyBloxFruit.lua
@@ -1,7 +1,1 @@
-getgenv().WebhookURLs = {
-    ["Dora"] = true,           -- Bật webhook của Dora
-    ["TuanAnhIos"] = true,     -- Bật webhook của Tuấn Anh Ios
-    -- Bạn có thể thêm nhiều webhook khác tại đây
-}
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TDDuym500/npmc_/refs/heads/main/NotifyBloxFruit.lua"))()
